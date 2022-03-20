a83399d8e638c591d409aa92bc21338a6a95bda1
hiro331 <rstu146>
2022-03-20 19:03:19 +0900

    clearButtonに削除機能を追加しました。

--- app/src/main/java/com/example/a100programapplication/TranslationFragment.java
+++ app/src/main/java/com/example/a100programapplication/TranslationFragment.java
@@ -7,20 +7,37 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TranslationFragment extends Fragment {
+    View rootview;
     public TranslationFragment() {
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_translation, container, false);
+        rootview =  inflater.inflate(R.layout.fragment_translation, container, false);
+
+        Button clear = rootview.findViewById(R.id.clearButton);
+        clear.setOnClickListener(new ButtonClickListener());
+
+        return rootview;
+    }
+
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view){
+            EditText input = rootview.findViewById(R.id.inputText);
+            input.setText("");
+        }
     }
 }
