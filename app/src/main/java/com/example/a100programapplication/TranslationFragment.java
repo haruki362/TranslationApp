7b02be5ef253b67f1b6d3c8055a0f385ebcf9938
user <haruki.120034@gmail.com>
2022-03-21 16:42:12 +0900

    TranslationApp コピー機能実装

--- app/src/main/java/com/example/a100programapplication/TranslationFragment.java
+++ app/src/main/java/com/example/a100programapplication/TranslationFragment.java
@@ -1,5 +1,9 @@
 package com.example.a100programapplication;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class TranslationFragment extends Fragment {
     View rootview;
@@ -30,6 +35,8 @@ public class TranslationFragment extends Fragment {
 
         ImageButton clear = rootview.findViewById(R.id.clearButton);
         clear.setOnClickListener(new ButtonClickListener());
+        ImageButton copy1 = rootview.findViewById(R.id.imageButton);
+        copy1.setOnClickListener(new ButtonClickListener());
 
         return rootview;
     }
@@ -38,7 +45,25 @@ public class TranslationFragment extends Fragment {
         @Override
         public void onClick(View view){
             EditText input = rootview.findViewById(R.id.inputText);
-            input.setText("");
+            TextView output = rootview.findViewById(R.id.textView);
+
+            int id = view.getId();
+            switch (id){
+                case R.id.clearButton:
+                    input.setText("");
+                    break;
+                case R.id.imageButton:
+                    String[] mimeType = new String[1];
+                    mimeType[0] = ClipDescription.MIMETYPE_TEXT_PLAIN;
+
+                    ClipData.Item item = new ClipData.Item(output.getText());
+
+                    ClipData CD = new ClipData(new ClipDescription("text", mimeType), item);
+
+                    ClipboardManager clipboard = (ClipboardManager)getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                    clipboard.setPrimaryClip(CD);
+                    break;
+            }
         }
     }
 }
